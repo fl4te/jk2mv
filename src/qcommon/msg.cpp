e82d83bbda4e0dfc26018cb4f56d538811b2c9f1
fau <4474909+aufau@users.noreply.github.com>
2025-05-04 12:57:05 +0200

    Fix wrong entity number in cl_shownet messages.
    Fixes #178
    Reported-by: @TomArrow

--- src/qcommon/msg.cpp
+++ src/qcommon/msg.cpp
@@ -1182,7 +1182,7 @@ void MSG_ReadDeltaEntity(msg_t *msg, entityState_t *from, entityState_t *to,
 	// just print the delta records`
 	if (cl_shownet->integer >= 2 || cl_shownet->integer == -1) {
 		print = 1;
-		Com_Printf("%3i: #%-3i ", msg->readcount, to->number);
+		Com_Printf("%3i: #%-3i ", msg->readcount, number);
 	} else {
 		print = 0;
 	}
